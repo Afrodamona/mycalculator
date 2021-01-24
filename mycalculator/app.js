d98bee7451e190628eb57ff27ac0a075c2a67b0e
Afrodamona <68506440+Afrodamona@users.noreply.github.com>
2021-01-24 08:10:39 +0100

    Add files via upload

---
+++ mycalculator/app.js
@@ -0,0 +1,9 @@
+
+var display = document.getElementById("display");
+function dis(val){
+    document.getElementById("display").value+=val;
+addEventListener(display, onclick){
+    console.log(val);
+}
+}
+
